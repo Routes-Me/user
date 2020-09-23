c0ec674f59b47c32ad1156f52692f32bc3c4ba73
Nirmal Patel <nirmal.patel@theonetechnologies.co.in>
2020-09-23 16:25:20 +0530

    Updated docker file and environment changes

--- UserService/Controllers/HomeController.cs
+++ UserService/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserService.Controllers
@@ -9,10 +10,19 @@ namespace UserService.Controllers
     [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Obsolete]
+        public readonly IHostingEnvironment _hostingEnv;
+
+        [Obsolete]
+        public HomeController(IHostingEnvironment hostingEnv)
+        {
+            _hostingEnv = hostingEnv;
+        }
         [HttpGet]
+        [Obsolete]
         public string Get()
         {
-            return "User service started successfully";
+            return "User service started successfully. Environment - " + _hostingEnv.EnvironmentName +"";
         }
     }
 }
