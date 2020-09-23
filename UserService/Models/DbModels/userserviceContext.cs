206340464c23ca57ee17163e7a0bff7479365165
Nirmal Patel <nirmal.patel@theonetechnologies.co.in>
2020-09-23 11:47:13 +0530

    Docker file changes

--- UserService/Models/DbModels/userserviceContext.cs
+++ UserService/Models/DbModels/userserviceContext.cs
@@ -20,15 +20,6 @@ namespace UserService.Models.DBModels
         public virtual DbSet<Users> Users { get; set; }
         public virtual DbSet<UsersRoles> UsersRoles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=MySQL@123;database=userservice", x => x.ServerVersion("8.0.20-mysql"));
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Phones>(entity =>
