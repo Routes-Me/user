c21344c02dbf78e25af6180fd69745dbad7e8f45
Nirmal Patel <nirmal.patel@theonetechnologies.co.in>
2020-10-12 19:54:29 +0530

    update country code

--- UserService/Repository/TwilioVerificationRepository.cs
+++ UserService/Repository/TwilioVerificationRepository.cs
@@ -23,8 +23,7 @@ namespace UserService.Repository
             try
             {
                 var verificationResource = await VerificationResource.CreateAsync(
-                    //to: "+965" + phone,
-                    to: "+91" + phone,
+                    to: "+965" + phone,
                     channel: "sms",
                     pathServiceSid: _config.VerificationSid
                 );
@@ -45,8 +44,7 @@ namespace UserService.Repository
             try
             {
                 var verificationCheckResource = await VerificationCheckResource.CreateAsync(
-                    //to: "+965" + phone,
-                    to: "+91" + phone,
+                    to: "+965" + phone,
                     code: code,
                     pathServiceSid: _config.VerificationSid
                 );
