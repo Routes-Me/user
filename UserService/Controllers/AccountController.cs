6c0bf38db00329eb934124a7e3962443f2348fc2
Wael Moughrbel <wmoughrbel@routesme.com>
2021-02-11 13:29:44 +0300

    Rename sessions api to authentications

--- UserService/Controllers/AccountController.cs
+++ UserService/Controllers/AccountController.cs
@@ -50,7 +50,7 @@ namespace UserService.Controllers
         }
 
         [HttpPost]
-        [Route("sessions")]
+        [Route("authentications")]
         public async Task<IActionResult> CreateSession(SigninModel signinModel)
         {
             (Users user, string token) = (new Users(), "");
