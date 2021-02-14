fbd34545256af849e0f78dba6621be141b18585c
Wael Moughrbel <wmoughrbel@routesme.com>
2021-02-14 14:03:43 +0300

    Added secret key for each environment

--- UserService/Models/Common/AppSettings.cs
+++ UserService/Models/Common/AppSettings.cs
@@ -8,6 +8,7 @@ namespace UserService.Models.Common
     public class AppSettings
     {
         public string Secret { get; set; }
+        public string SecretKey { get; set; }
         public string ValidAudience { get; set; }
         public string ValidIssuer { get; set; }
         public string SessionTokenIssuer { get; set; }
