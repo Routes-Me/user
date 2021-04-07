369a0369ed5b2ef0d0390646c6d2d390085791e9
Wael Moughrbel <wmoughrbel@routesme.com>
2021-04-07 14:35:29 +0300

    Remove duplicate Controllers to only support versioned APIs

--- UserService/Models/Common/AppSettings.cs
+++ UserService/Models/Common/AppSettings.cs
@@ -2,17 +2,6 @@
 {
     public class AppSettings
     {
-        public string Secret { get; set; }
-        public string AccessSecretKey { get; set; }
-        public string RefreshSecretKey { get; set; }
-        public string ValidAudience { get; set; }
-        public string ValidIssuer { get; set; }
-        public string SessionTokenIssuer { get; set; }
         public string Host { get; set; }
-        public string IVForAndroid { get; set; }
-        public string KeyForAndroid { get; set; }
-        public string IVForDashboard { get; set; }
-        public string KeyForDashboard { get; set; }
-        public string RoutesAppUrl { get; set; }
     }
 }
